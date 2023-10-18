d11a56a8c85dfa806fd08ce9934dc9576d458f08
Canalp <canalpbelgutay@gmail.com>
2023-10-18 12:38:50 +0200

    commit test

--- dosyadeneme.py
+++ dosyadeneme.py
@@ -1,4 +1,8 @@
 import os
 import shutil
 
-source_path = "C:\Users\canal\Desktop\deneme.txt"
+source_path = "C:/Users/canal/Desktop/deneme.txt"
+
+destination_path = "C:/Users/canal/Desktop/shutil"
+
+shutil.move(source_path, destination_path)
