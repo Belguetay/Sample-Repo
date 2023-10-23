2b83f83cfa4e5b56ffbc4f5e0cbd6d2603ba6a58
Canalp <canalpbelgutay@gmail.com>
2023-10-23 11:47:13 +0200

    commit

--- operator.py
+++ operator.py
@@ -1,6 +1,6 @@
 import operator
 
-# # # # #
+# # # # # conda environmentta calisiyor
 print("Experiment 1")
 
 data = [{'name': 'Alice', 'age': 30}, {'name': 'Bob', 'age': 25}, {'name': 'Charlie', 'age': 35}]
---
+++ python environment.py
@@ -0,0 +1,11 @@
+# List Installed Packages:
+# write it in the terminal
+# pip list
+
+# View Configuration Information:
+import sys
+print(sys.prefix)  # Path to the Python environment
+print(sys.version)  # Python version
+
+# Check Installed Modules:
+help('modules')
