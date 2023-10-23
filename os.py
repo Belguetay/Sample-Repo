206568e761788e25a58dbdc960644afbd4b9cd3d
Canalp <canalpbelgutay@gmail.com>
2023-10-23 09:15:31 +0200

    initial commit

--- os.py
+++ os.py
@@ -1,1 +1,13 @@
-d
+import os
+
+current_directory = os.getcwd()
+print(f"Hey Canalp, this is the current_directory: {current_directory}")
+
+new_directory = "C:/Users/canal/Desktop/os/new directory brought to you by the os module"
+os.mkdir(new_directory)
+print(f"Created a new directory: {new_directory}")
+
+directory_contents = os.listdir("C:/Users/canal/Pictures/Screenshots")
+print("Directory Contents:")
+for item in directory_contents:
+    print(item)
