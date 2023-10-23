a0b47afdaa31c1035f74855ba9b8058e4de17195
Canalp <canalpbelgutay@gmail.com>
2023-10-23 12:42:02 +0200

    commit

---
+++ numpytest.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# Create a 1D array
+arr1 = np.array([1, 2, 3])
+
+# Create a 2D array (matrix)
+arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+
+# Create an array of zeros
+zeros = np.zeros((2, 3))
+
+# Create an array of ones
+ones = np.ones((3, 2))
+
+# Create an identity matrix
+identity = np.eye(3)
+
+print("1D Array:", arr1)
+print("2D Array:", arr2)
+print("Zeros Array:", zeros)
+print("Ones Array:", ones)
+print("Identity Matrix:", identity)
